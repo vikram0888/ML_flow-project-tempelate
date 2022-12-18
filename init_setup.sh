3a00f9167201899bffa17331de5bca252cf60f32
vikram0888 <100748938+vikram0888@users.noreply.github.com>
2022-12-18 16:16:31 +0530

    init_setup

--- init_setup.sh
+++ init_setup.sh
@@ -1,4 +1,5 @@
 #it will do automate the setup
+##write the line in terminal to initiate the below line of code --> $bash init_setup.sh
 conda create --prefix ./env python=3.7 -y
 source activate ./env
 pip install -r requirements.txt
